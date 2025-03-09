e713f169949d71c9b9587f0483e159a336157110
AndreBezerra19 <andrebezerra19@yahoo.com.br>
2025-03-09 15:11:17 -0300

    WIP desafio_2 sensores iot com kafka

---
+++ sensores_iot/producer.py
@@ -0,0 +1,47 @@
+import json
+import time
+import random
+from confluent_kafka import Producer
+from faker import Faker
+
+# Faker init
+fake = Faker()
+
+# Kafka configs
+kafka_config = {
+    "bootstrap.servers": "localhost:9092"  # Ajuste conforme necessário
+}
+
+producer = Producer(kafka_config)
+topic = "iot_sensors"
+
+# data generation function
+def generate_sensor_data():
+    sensor_id = random.randint(1, 20)
+    temperature = round(random.uniform(0.0, 40.0), 2)
+    humidity = round(random.uniform(20.0, 80.0), 2)
+    pressure = round(random.uniform(900, 1100), 2)
+    location = fake.city()  # make random city
+    
+    data = {
+        "sensor_id": sensor_id,
+        "temperature": temperature,
+        "humidity": humidity,
+        "pressure": pressure,
+        "location": location,
+        "timestamp": time.time()
+    }
+    
+    return json.dumps(data)
+
+# send data function
+def send_to_kafka():
+    while True:
+        message = generate_sensor_data()
+        producer.produce(topic, key=str(random.randint(1, 100)), value=message)
+        producer.flush()
+        print(f"Sent: {message}")
+        time.sleep(2)  # creates a gap between sensor readings
+
+if __name__ == "__main__":
+    send_to_kafka()
