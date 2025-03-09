28079cbcbe4c3f7358b839f57ba1b5e87e05eb7b
AndreBezerra19 <andrebezerra19@yahoo.com.br>
2025-03-09 13:31:27 -0300

    solution with pandas, pyspark and duckdb

--- diario_de_bordo/duckdb_solution/read_data.py
+++ diario_de_bordo/duckdb_solution/read_data.py
@@ -1,6 +1,8 @@
 # Dependencies
 import duckdb
 import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import substring, count, col, when, desc, min, max, sum, round, to_date, date_format
 
 # Parameters
 db_path = "/Users/andrebezerra/Desktop/Dev/DesafiosCodeElevate/db_desafios.duckdb"
@@ -13,31 +15,59 @@ con = duckdb.connect(db_path)
 # Read table using pandas
 df = con.execute(f"SELECT * FROM {schema}.{table_name}").fetchdf()
 
-##### ETL #####
-
 # date information and conversion
-df["DATA_INICIO"] = df["DATA_INICIO"].str.split(" ").str[0]  # Mantém apenas a data
+df["DATA_INICIO"] = df["DATA_INICIO"].str.split(" ").str[0]  # get only date
 df["DATA_INICIO"] = pd.to_datetime(df["DATA_INICIO"], format="%m-%d-%Y").dt.strftime("%Y-%m-%d")
 
-# Agrupamento e agregações
-df_resultado = df.groupby("DATA_INICIO").agg(
-    QT_CORR=pd.NamedAgg(column="LOCAL_INICIO", aggfunc=lambda x: x.notna().sum()),  # Contagem total de viagens (não nulas)
-    QT_CORR_NEG=pd.NamedAgg(column="CATEGORIA", aggfunc=lambda x: (x == "Negocio").sum()),  # Contagem de viagens de "Negocio"
-    QT_CORR_PESS=pd.NamedAgg(column="CATEGORIA", aggfunc=lambda x: (x == "Pessoal").sum()),  # Contagem de viagens de "Pessoal"
-    VL_MAX_DIST=pd.NamedAgg(column="DISTANCIA", aggfunc="max"),  # Maior distância percorrida no dia
-    VL_MIN_DIST=pd.NamedAgg(column="DISTANCIA", aggfunc="min"),  # Menor distância percorrida no dia
-    VL_AVG_DIST=pd.NamedAgg(column="DISTANCIA", aggfunc=lambda x: round(x.sum() / len(x), 2)),  # Distância média do dia
-    QT_CORR_REUNI=pd.NamedAgg(column="PROPOSITO", aggfunc=lambda x: (x == "Reunião").sum()),  # Contagem de viagens para "Reunião"
-    QT_CORR_NAO_REUNI=pd.NamedAgg(column="PROPOSITO", aggfunc=lambda x: x.notna().sum() - (x == "Reunião").sum())  # Viagens com propósito diferente de "Reunião"
-).reset_index()
+con.close()
 
-# Ordenando os dados do mais recente para o mais antigo
-df_resultado = df_resultado.sort_values(by="DATA_INICIO", ascending=False)
+##### ETL #####
+
+# Spark session
+spark = SparkSession.builder.appName("DuckDB_PySpark").getOrCreate()
+
+# Converter o DataFrame Pandas para PySpark
+df_spark = spark.createDataFrame(df)
+
+# data aggregation
+df_resultado = df_spark.groupBy("DATA_INICIO").agg(
+    count(col("LOCAL_INICIO")).alias("QT_CORR"),        # total travels
+    count(when(col("CATEGORIA") == "Negocio", True)).alias("QT_CORR_NEG"),      # Count travels like 'Negocio'
+    count(when(col("CATEGORIA") == "Pessoal", True)).alias("QT_CORR_PESS"),     # Count travels like 'Pessoal'
+    max(col("DISTANCIA")).alias("VL_MAX_DIST"),     # longest distance of the day
+    min(col("DISTANCIA")).alias("VL_MIN_DIST"),     # shortest distance of the day
+    round(sum(col("DISTANCIA")) / count(col("DATA_INICIO")), 2).alias("VL_AVG_DIST"),       # average total distance
+    count(when(col("PROPOSITO").isin("Reunião"), True)).alias("QT_CORR_REUNI"),     # Travels for the purpose of "Reunião".
+    count(when((col("PROPOSITO").isNotNull()) & (~col("PROPOSITO").isin("Reunião")), True)).alias("QT_CORR_NAO_REUNI")      # Travels with a stated purpose other than "Reunião".  
+).orderBy(col("DATA_INICIO").desc())
 
 # Save into DuckDB on correct schema
-con.register("temp_table", df_resultado)  # Register DF like temp table
-con.execute("CREATE TABLE IF NOT EXISTS diario_de_bordo.s_info_corridas_do_dia AS SELECT * FROM temp_table")
+pd_resultados = df_resultado.toPandas()
+con = duckdb.connect(db_path)
 
-# Close connection
+# Create table
+con.execute("""
+    CREATE TABLE IF NOT EXISTS diario_de_bordo.s_info_corridas_do_dia (
+        DATA_INICIO DATE,
+        QT_CORR INTEGER,
+        QT_CORR_NEG INTEGER,
+        QT_CORR_PESS INTEGER,
+        VL_MAX_DIST FLOAT,
+        VL_MIN_DIST FLOAT,
+        VL_AVG_DIST FLOAT,
+        QT_CORR_REUNI INTEGER,
+        QT_CORR_NAO_REUNI INTEGER
+    )
+""")
+
+con.register("temp_table", pd_resultados)  # Register DF like temp table
+
+# insert data into table
+con.execute("""
+    INSERT INTO diario_de_bordo.s_info_corridas_do_dia 
+    SELECT * FROM temp_table
+""")
+
 con.close()
+
 print("Table saved successfully in DuckDB! 🚀")
